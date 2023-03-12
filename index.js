536c6a671550785aa2615815a77f84846d43a541
shawn <parvonisyego@gmail.com>
2023-03-12 20:34:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let names = [`steven`, `kiptoo`, `yabann`]
+message = [``]
+
+function writeCards(names, event){
+    for(let i = 0; i < names.length; i++){
+        message = [...message, `Thank you, ${names[i]}, for the wonderful ${event} gift!`]
+    debugger
+    }
+
+    return message
+}
+writeCards(names, "birthday")
+
+function countDown(){
+    let n = 20;
+    while (n > 0){
+        console.log(n)
+        n--
+    }
+}
+countDown()
